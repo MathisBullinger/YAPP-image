efd607bd2e3980c3ca475aae3eb165d0be9f0789
MathisBullinger <mathis.bullinger@code.berlin>
2019-09-20 15:59:05 +0200

    initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+const config = {
+  extends: ["eslint:recommended", "prettier/react"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["prettier", "@typescript-eslint"],
+  env: {
+    node: true,
+    es6: true
+  },
+  parserOptions: {
+    parser: "babel-eslint",
+    project: path.resolve(__dirname, "./tsconfig.json"),
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  globals: {
+    module: true,
+    process: true,
+    require: true,
+    __dirname: "readonly"
+  },
+  rules: {
+    semi: ["error", "never"],
+    "@typescript-eslint/no-unused-vars": "error"
+  }
+};
+
+module.exports = config;
