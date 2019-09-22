42ae6dbf5dadff2e33fdfdcf964372b20ac75814
MathisBullinger <mathis.bullinger@code.berlin>
2019-09-22 19:32:30 +0200

    log new items

--- src/handler.ts
+++ src/handler.ts
@@ -1,1 +1,10 @@
-export const image = async (event, context) => {}
+export const image = async (event, context) => {
+  const newItems = event.Records.filter(
+    ({ eventName }) => eventName === 'INSERT'
+  ).map(event => event.dynamodb.NewImage)
+
+  console.log(newItems.length, 'new items')
+  newItems.forEach(console.dir)
+
+  return { statusCode: 200 }
+}
