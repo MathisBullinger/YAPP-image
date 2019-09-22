06f0c8ef5e332946a6f8c61520d5ba1db3b528aa
MathisBullinger <mathis.bullinger@code.berlin>
2019-09-23 00:54:12 +0200

    (ref): rewrite resize function

--- src/image.ts
+++ src/image.ts
@@ -1,4 +1,3 @@
-import fs from 'fs'
 import axios from 'axios'
 import sharp from 'sharp'
 
@@ -6,28 +5,28 @@ export const resize = async (url: string) => {
   const sizes = [256, 512, 1024]
   const formats = ['jpeg', 'webp']
 
-  axios({ method: 'get', url, responseType: 'arraybuffer' }).then(
-    ({ data }) => {
-      const image = sharp(data)
-      sizes.forEach(size => {
-        image.metadata().then(meta => {
-          const tmp = image.resize({
-            [meta.width >= meta.height ? 'width' : 'height']: size,
-            kernel: sharp.kernel.cubic,
-          })
-          formats.forEach(format =>
-            tmp[format]()
-              .toBuffer()
-              .then(data => {
-                const fsStream = fs.createWriteStream(
-                  `img/img${size}.${format}`
-                )
-                fsStream.write(data)
-                fsStream.end()
-              })
-          )
-        })
-      })
-    }
+  const { data } = await axios({
+    method: 'get',
+    url,
+    responseType: 'arraybuffer',
+  })
+
+  const image = sharp(data)
+  const meta = await image.metadata()
+
+  const resize = (img: sharp.Sharp, size: number) =>
+    img.clone().resize(null, null, {
+      [meta.width >= meta.height ? 'width' : 'height']: size,
+      kernel: sharp.kernel.cubic,
+    })
+
+  const sized = sizes.map(size => resize(image, size))
+
+  await Promise.all(
+    formats.map(format =>
+      sized.map((img, i) =>
+        img[format]().toFile(`img/img${sizes[i]}.${format}`)
+      )
+    )
   )
 }
